917379062953dff00286e6406355de98e3a2af33
Micaias-star <micaiassilva021@gmail.com>
2020-01-08 23:20:39 -0300

    Aula-3.1 08/01/2020

---
+++ Aula3.1.php
@@ -0,0 +1,19 @@
+<?php
+
+  $nome ="Hcode";                                  //strings
+  $site = 'www.hcode.com.br';                     //string
+  $ano  =  1998;                                 //Inteiro
+  $salario = 5500.99;                           //Ponto flutuante
+  $bloqueado = false;                          //Booleano
+  ///////////////////////////////
+  $frutas = array("abacaxi", "Laranja");     //array
+ //cho $frutas[1];
+
+  $nascimento = new DateTime();             //Objeto
+//var_dump($nascimento);                 // Coloquei esse comando para comprovar visualmente o tipo de dados
+/////////////////////////////////
+  $arquivo = fopen ("1.png","r");     //Variável do tipo resource
+  var_dump($arquivo);
+//$nulo = null                       //Variável nula, ou seja não tem valor
+//$vazio =""                     //vazio, ou seja, tem espaço reservado na memória, mas não há nenhuma informação.
+?>
