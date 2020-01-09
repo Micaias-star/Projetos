f75365101c90e6c302031ec596fcf7e3c0aa51b5
Micaias-star <micaiassilva021@gmail.com>
2020-01-08 22:32:40 -0300

    Aula-3 08/01/2020

--- Aula3.php
+++ Aula3.php
@@ -1,7 +1,15 @@
 <?php
 
- $nome ="Teste";
+ $nome ="Micaias";
+ $sobrenome = "Silva";
+ $nomeCompleto = $nome . " " . $sobrenome; // Usa-se o . para concatenar as variáveis.
 
-var_dump($nome)
+ echo $nomeCompleto;
+ exit; //O php ira para de executar aqui
+  
+ unset($nome);          //Limpa os dados da variável
+ if (isset($nome)){     
+ 	echo $nome;
+ }                      // O comando isset a cima verifica se existe dados nas variável em questão
 
 ?>
