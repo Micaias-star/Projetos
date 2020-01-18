07973fef6e189df12aeba640d379efee33323d51
Micaias Silva <59585916+Micaias-star@users.noreply.github.com>
2020-01-17 22:12:45 -0300

    Aula6; 16/01

--- Strings/Aula6-Edição de textos.php
+++ Strings/Aula6-Edição de textos.php
@@ -4,28 +4,28 @@ $nome = "Joao Rangel-Hcode";
 
 $nome = strtoupper ($nome);
 
-echo $nome;
+echo $nome;                    //JOAO RANGEL-HCODE
 
 $nome = strtolower ($nome);
 
-echo "<br>";
+echo "<br>";                  
 
-echo $nome;
-
-echo "<br>";
-
-echo ucfirst($nome);
+echo $nome;                   //joao rangel-hcode
 
 echo "<br>";
 
-echo ucwords($nome);
+echo ucfirst($nome);          //Joao rangel-hcode
+
+echo "<br>";
+
+echo ucwords($nome);          //Joao Rangel-hcode
 
 echo "<br>";
 
 $nome = str_replace ("o", "0", $nome); 
 $nome = str_replace ("e", "3", $nome); 
 
-echo $nome
+echo $nome                   //j0h0 rang3l-hc0d3
 
 
 
